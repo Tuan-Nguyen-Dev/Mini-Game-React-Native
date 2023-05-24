b8d6cc3b684729477703bdb50da3261dcbdbe8a4
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2023-05-24 12:03:38 +0700

    init project

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,48 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import React from 'react'
+import PrimaryButton from '../components/PrimaryButton'
+
+const StartGameScreen = () => {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numnerInput}
+                maxLength={2}
+                keyboardType='number-pad'
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
+            <PrimaryButton>Làm Mới</PrimaryButton>
+            <PrimaryButton>Xác nhận</PrimaryButton>
+
+        </View>
+    )
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#4e0329',
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numnerInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
