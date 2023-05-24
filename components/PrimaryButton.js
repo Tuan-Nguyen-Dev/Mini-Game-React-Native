7ff415b43933db96d36c2e683cd62652e7e8442a
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2023-05-24 13:28:22 +0700

    change color button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
         elevation: 2,
     },
     buttonText: {
-        color: 'white',
+        color: '#333',
         textAlign: 'center'
     },
     pressed: {
